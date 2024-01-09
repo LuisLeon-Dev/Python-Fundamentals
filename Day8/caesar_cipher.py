830b8ef6f76b69b55408f0abbed380ccfbfb5547
LuisLeon-Dev <lleon5073@gmail.com>
2024-01-08 23:30:08 -0600

    fixing errors and improve the logic

--- Day8/caesar_cipher.py
+++ Day8/caesar_cipher.py
@@ -1,18 +1,38 @@
 from alphabet import alphabet
+from art import logo
 
+print(logo)
+# Determine the user's desired direction
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
+text = input("Type your message to decode:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 
-def encrypt(text_to_encrypt, shift_number):
-    encrypted_text = ""
-    for char in text_to_encrypt:
-        position = alphabet.index(char)
-        new_position = position + shift_number
-        new_char = alphabet[new_position]
-        encrypted_text += new_char
-    return print(encrypted_text)
+def caesar(start_text, shift_amount, cipher_direction):
+    end_text = ""
+    if cipher_direction == "decode":
+        shift_amount *= -1
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_position = position + shift_amount
+            end_text += alphabet[new_position]
+        else:
+            end_text += char
+    print(f"Here's the {cipher_direction}d result: {end_text}")
 
 
-encrypt(text_to_encrypt=text, shift_number=shift)
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
